1ae5bdf1b315c06ec27706e90ab5d8efc44b10d2
Leon Cruz <leon.cruz.teixeira@gmail.com>
2020-10-31 19:05:16 -0300

    Cadastro do usuário

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import redirect, render
 from django.views.decorators.http import require_http_methods
 
 from .forms import SignupForm
@@ -9,3 +10,15 @@ from .forms import SignupForm
 def index(request):
     signup_form = SignupForm()
     return render(request, "accounts/index.html", {"form": signup_form})
+
+
+@require_http_methods(["POST"])
+def signup(request):
+    signup_form = SignupForm(request.POST)
+
+    if signup_form.is_valid():
+        messages.success(request, "Cadastro realizado com sucesso")
+        signup_form.save()
+        return redirect("accounts:index")
+    else:
+        return redirect("accounts:index")
