8efd618cc37368ab1e0d733a2898f85ee099ee85
Leon Cruz <leon.cruz.teixeira@gmail.com>
2020-10-31 23:24:35 -0300

    Criação do modelo do usuário
    	modified:   accounts/forms.py

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,13 +1,16 @@
 from django import forms
-from django.contrib.auth.models import User
+
+from .models import User
 
 
 class SignupForm(forms.ModelForm):
     class Meta:
         model = User
-        fields = ["username", "email", "password"]
+        fields = ["full_name", "username", "email", "password"]
+
         widgets = {
+            "full_name": forms.TextInput(attrs={"class": "input"}),
             "username": forms.TextInput(attrs={"class": "input"}),
-            "email": forms.TextInput(attrs={"class": "input"}),
+            "email": forms.EmailInput(attrs={"class": "input"}),
             "password": forms.PasswordInput(attrs={"class": "input"}),
         }
--- gallery/settings.py
+++ gallery/settings.py
@@ -123,3 +123,5 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 STATICDIRS = os.path.join(BASE_DIR, "static")
+
+AUTH_USER_MODEL = "accounts.User"
