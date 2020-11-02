616257f39f8e8da00a2b3c0777222f7d66c00abe
Leon Cruz <leon.cruz.teixeira@gmail.com>
2020-11-02 18:32:28 -0300

    Incio da criação do layout

--- user_profile/views.py
+++ user_profile/views.py
@@ -5,4 +5,5 @@ from django.shortcuts import render
 # Create your views here.
 @login_required(login_url="accounts:signin")
 def index(request):
-    return render(request, "user_profile/index.html")
+    lista = list(range(1, 11))
+    return render(request, "user_profile/index.html", {"lista": lista})
